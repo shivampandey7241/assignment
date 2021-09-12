f2d67b45870a87fbf6269a78a3ca2d15024d68b1
shivampandey7241 <shivampandey7241@gmail.com>
2021-09-12 14:45:47 +0530

    first

---
+++ javaprogram/src/javaprogram/simpleifelse.java
@@ -0,0 +1,16 @@
+package javaprogram;
+
+public class simpleifelse {
+
+	public static void main(String[] args) {
+		
+		int vx = 16;
+		int vy = 23;
+		System max = (vx > vy) ? "x":"y";
+		System.out.println(max);
+		
+	    
+
+	}
+
+}
---
+++ javaprogram/src/simple.java
@@ -0,0 +1,11 @@
+public class simple{
+	public static void main(String[]args) {
+		byte x = 4;
+		byte y = 9;
+		if(x>y) {
+			System.out.println("x is bigger");
+		}   else {
+			System.out.println("x is smaller");
+		}
+	}
+}
