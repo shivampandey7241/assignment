63c0c1b9b25bb2efc7b7c906ef072ea5b584ba92
shivampandey7241 <shivampandey7241@gmail.com>
2021-09-12 14:59:14 +0530

    third

--- javaprogram/src/javaprogram/simpleifelse.java
+++ javaprogram/src/javaprogram/simpleifelse.java
@@ -4,9 +4,9 @@ public class simpleifelse {
 
 	public static void main(String[] args) {
 		
-		int vx = 16;
-		int vy = 23;
-		System max = (vx > vy) ? "vx":"vy";
+		byte vx = 16;
+		byte vy = 23;
+		byte max = (vx > vy) ? "vx":"vy";
 		System.out.println("Bigger number is "+max);
 		
 	    
