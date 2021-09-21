d568c0a41525f25203a538986f040cfb8c47b125
shivampandey7241 <shivampandey7241@gmail.com>
2021-09-21 17:18:43 +0530

    Sep20

--- Practice2/PatternSep20Ques1.java
+++ Practice2/PatternSep20Ques1.java
@@ -7,6 +7,17 @@ public class PatternSep20Ques1 {
 //		  * * * 
 //		 * * * * 
 //		* * * * * 
+                int n = 5;
+		for(int i = 0; i<n; ++i) {
+//		System.out.println(1);
+			for(int j = n-i; j>1; --j) {
+				System.out.print(" ");
+			}
+			for (int x =0; x<=i; ++x) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
 		
 		
 		
